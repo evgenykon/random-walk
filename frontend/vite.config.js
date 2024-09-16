e506f08b72c02a86ea8766dafe7b4aae306a2f84
evgeny.konoplev <effusps@gmail.com>
2024-09-16 09:31:58 +0300

    released

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -28,6 +28,7 @@ export default defineConfig({
       globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
       cleanupOutdatedCaches: true,
       clientsClaim: true,
+      maximumFileSizeToCacheInBytes: 3 * 1024 * 1024,
     },
 
     devOptions: {
