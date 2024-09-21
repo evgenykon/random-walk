a9ead332774b6fdc4d876573ac6809afd822413e
evgeny.konoplev <effusps@gmail.com>
2024-09-21 13:22:14 +0300

    icons

--- frontend/src/webapi/Db.js
+++ frontend/src/webapi/Db.js
@@ -1,4 +1,4 @@
-import { Dexie } from 'dexie'
+// import { Dexie } from 'dexie'
 
 class Db {
     dbName = 'randMarathonDb'
@@ -7,40 +7,47 @@ class Db {
     dbPointer
 
     constructor() {
-        this.dbPointer = new Dexie(this.dbName);
-        this.dbPointer.version(1).stores({
-            tracks: 'id++, trackId, data'
-        })
+        // this.dbPointer = new Dexie(this.dbName);
+        // this.dbPointer.version(1).stores({
+        //     tracks: 'id++, trackId, data'
+        // })
         console.log('IndexDb inited')
     }
 
     async clearDb() {
-        return (await this.dbPointer.tracks.clear());
+        // return (await this.dbPointer.tracks.clear());
+        localStorage.clear()
     }
 
     async loadTrack(trackId) {
-        return (await this.dbPointer.tracks.where({trackId: String(trackId)}).toArray());
+        // return (await this.dbPointer.tracks.where({trackId: String(trackId)}).toArray());
+        const data = localStorage.getItem(`track.${trackId}`)
+        if (data) {
+            return JSON.parse(data)
+        }
     }
 
     async dropTrack(trackId) {
-        return new Promise((resolve, reject) => {
-            const db = this.dbPointer.tracks
-            this.dbPointer.transaction('rw', db, async () => {
-                return db.where("trackId").equals(trackId).delete();
-            }).then(() => {
-                resolve()
-            }).catch(reject)
-        });
+        // return new Promise((resolve, reject) => {
+        //     const db = this.dbPointer.tracks
+        //     this.dbPointer.transaction('rw', db, async () => {
+        //         return db.where("trackId").equals(trackId).delete();
+        //     }).then(() => {
+        //         resolve()
+        //     }).catch(reject)
+        // });
+        localStorage.removeItem(`track.${trackId}`)
     }
 
     async saveTrack(trackId, data) {
-        return new Promise((resolve) => {
-            this.dbPointer.transaction('rw', this.dbPointer.tracks, () => {
-                this.dbPointer.tracks.add({trackId: String(trackId), data: JSON.stringify(data) }).then(() => {
-                    resolve();
-                })
-            })
-        });
+        // return new Promise((resolve) => {
+        //     this.dbPointer.transaction('rw', this.dbPointer.tracks, () => {
+        //         this.dbPointer.tracks.add({trackId: String(trackId), data: JSON.stringify(data) }).then(() => {
+        //             resolve();
+        //         })
+        //     })
+        // });
+        localStorage.setItem(`track.${trackId}`, JSON.stringify(data))
     }
 }
 
